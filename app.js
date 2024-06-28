7d7dca0e35af71d6f268d3afa9286ad7c7eb925f
Eslam ELshaer <161819012+EslamELshaer0@users.noreply.github.com>
2024-06-28 12:10:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function sendToWatsapp(){
+let number ="+92     ";
+
+let name =doument.getElementByTd('name').value;
+let email=doument.getElementByTd('email').value;
+let message=doument.getElementByTd('message').value;
+ var url ="https://wa.me/" +number +"?text"
+ + "Name :" +name+ "%0a"
+ + "Name :" +email+ "%0a"
+ + "Name :" +message+ "%0a%a"
+
+ window.open( ur,'blank').focus();
+}
+ 
+
